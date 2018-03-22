a90e9f14c39bbe067e8dd39aa01c81e8f88a2c95
masataka <ar.tec.masa@gmail.com>
2018-03-23 01:41:35 +0900

    add render_template

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return 'Index Page'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
